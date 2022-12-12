11f3d37fac8e7b08a13e5609d6fb0aac195b1957
winterofmaple2016 <690566137@qq.com>
2022-12-12 18:01:31 +0800

    update file directory

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ PLATFORM="amd64"
 
 DH_IMAGE="${HOST}/${USER}/${DH}"
 DH_IMAGE_TAGGED="${DH_IMAGE}:${TAGS}"
-docker build -t=${DH_IMAGE_TAGGED} -f build/containers/Dockerfile.onvif-discovery . --build-arg PLATFORM=${PLATFORM} --build-arg CROSS_BUILD_TARGET=${target};
+docker build -t=${DH_IMAGE_TAGGED} -f containers/Dockerfile.onvif-discovery . --build-arg PLATFORM=${PLATFORM} --build-arg CROSS_BUILD_TARGET=${target};
 docker push ${DH_IMAGE_TAGGED};
